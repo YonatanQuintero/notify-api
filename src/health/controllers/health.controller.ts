ebf14817e5f59a813ee0cd5d9b649344220912ff
YonatanQuintero <yonatan.a.quintero.r@gmail.com>
2025-02-12 17:28:59 -0500

    feat: Global Exception Handling & i18n Error Messages

--- src/health/controllers/health.controller.ts
+++ src/health/controllers/health.controller.ts
@@ -1,4 +1,6 @@
-import { Controller, Get } from '@nestjs/common';
+import { BadRequestException, Controller, Get, InternalServerErrorException } from '@nestjs/common';
+import { InvalidIPAddressError } from 'src/app/errors/invalid-ip-address.error';
+import { DomainError } from 'src/app/primitives/domain-error';
 import { Authentication } from 'src/authentication/decorators/authentication.decorator';
 import { AuthenticationType } from 'src/authentication/enums/authentication-type.enum';
 
@@ -12,5 +14,18 @@ export class HealthController {
             uptime: process.uptime(),
             version: 'v1'
         };
+    }
+
+    @Get("/error")
+    error() {
+        try {
+            throw new InvalidIPAddressError("xxxx");
+        } catch (error) {
+            if (error instanceof DomainError) {
+                throw new BadRequestException(error.code, error.message);
+            } else {
+                throw new InternalServerErrorException(error.message);
+            }
+        }
     }
 }
